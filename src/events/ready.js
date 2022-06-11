9ae3a1b505cbb84b613d089e9e880079ea2ea347
Henonnnn <46067390+Mmoinana@users.noreply.github.com>
2022-06-11 08:47:16 +0200

    testt

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-		console.log(`Ready! Loggadined in as ${client.user.tag}`);
+		console.log(`Ready! Loggadinedjeanaimareuh in as ${client.user.tag}`);
 	},
 };
