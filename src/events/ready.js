55a2b6acf29a221376c58f43639b785c2cad835f
Henonnnn <46067390+Mmoinana@users.noreply.github.com>
2022-06-11 08:41:34 +0200

    first committ

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-		console.log(`Ready! Loggadined in as ${client.user.tag}`);
+		console.log(`Ready! Loggadinedencore in as ${client.user.tag}`);
 	},
 };
